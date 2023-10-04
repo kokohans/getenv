9e84001785664c86a4c31562e403f913e9a4a201
kokohans <hw.wiranata@gmail.com>
2023-10-04 12:39:05 +0700

    add: helper module

---
+++ src/bin/helper.py
@@ -0,0 +1,17 @@
+import platform
+import socket
+import os
+
+
+def get_info():
+    info = {}
+    suptime = os.popen('uptime').read()[:-1].split(" ")[3].rstrip(",").split(":")
+
+    info['hostname'] = socket.getfqdn()
+    info['addr'] = socket.gethostbyname(socket.gethostname())
+    info['uptime'] = f"up {suptime[0]} hours, {suptime[1]} minutes"
+    info['cpu'] = os.popen('cat /proc/cpuinfo  | grep -m1 "model name"').read()[:-1].split(":")[1].lstrip()
+    info['platform'] = platform.uname()
+    info['python-version'] = platform.python_version()
+
+    return info
