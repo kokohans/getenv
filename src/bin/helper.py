4201076b030f13b509b823bbed68352b4bb9b448
kokohans <hw.wiranata@gmail.com>
2023-10-12 12:25:03 +0700

    fix: array out of range uptime

--- src/bin/helper.py
+++ src/bin/helper.py
@@ -9,7 +9,13 @@ def get_info():
 
     info['hostname'] = socket.getfqdn()
     info['addr'] = socket.gethostbyname(socket.gethostname())
-    info['uptime'] = f"up {suptime[0]} hours, {suptime[1]} minutes"
+
+    try:
+        info['uptime'] = f"up {suptime[0]} hours, {suptime[1]} minutes"
+    except IndexError:
+        info['uptime'] = f"up {suptime[0]} hours, 0 minutes"
+
+
     info['cpu'] = os.popen('cat /proc/cpuinfo  | grep -m1 "model name"').read()[:-1].split(":")[1].lstrip()
     info['platform'] = platform.uname()
     info['python-version'] = platform.python_version()
