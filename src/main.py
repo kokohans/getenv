359dcfafa89dfd9773a749e74174f41e9b853930
kokohans <hw.wiranata@gmail.com>
2023-10-18 21:12:35 +0700

    feat: add switch case system status

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status
 from bin import helper
 
 app = FastAPI()
@@ -8,3 +8,18 @@ app = FastAPI()
 async def root():
     node_info = helper.get_info()
     return node_info
+
+@app.get("/healthz")
+async def get_health_status(response: Response):
+
+    node_health = helper.get_health()
+
+    match node_health['system_status']:
+        case 'healthy':
+            response.status_code = status.HTTP_200_OK
+        case 'unhealthy':
+            response.status_code = status.HTTP_503_SERVICE_UNAVAILABLE
+        case _:
+            response.status_code = status.HTTP_503_SERVICE_UNAVAILABLE
+
+    return node_health
